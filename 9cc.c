9afc734517e5986f2a270cf9c124aeb465849390
Jabelic <hibiki990523@gmail.com>
2020-10-18 11:02:37 +0000

    error -> error_at: represent the error position

--- 9cc.c
+++ 9cc.c
@@ -5,13 +5,12 @@
 #include <string.h>
 #include <stdarg.h>
 
-
 // トークンの種類 列挙型
-typedef enum {
+typedef enum{
     TK_RESERVED, // 記号
-    TK_NUM,  // 整数トークン
-    TK_EOF, // 入力の終わりを表すトークン
-} Tokenkind; //0,1,2
+    TK_NUM,      // 整数トークン
+    TK_EOF,      // 入力の終わりを表すトークン
+} Tokenkind;     //0,1,2
 //TAG名は省略している
 
 typedef struct Token Token; //Tokenという名前のTokenという型を定義
@@ -20,31 +19,36 @@ typedef struct Token Token; //Tokenという名前のTokenという型を定義
 // トークン型
 // ここが構造体
 struct Token{
-    Tokenkind kind; // トークンの型  kind.TK_NUM = 1;と言うふうにできる？
-    Token *next;   // 次の入力トークン
-    int val;       // kindがTK_NUMの場合, その数値
-    char *str;     // トークン文字列
+    Tokenkind kind; // トークンの型
+    Token *next;    // 次の入力トークン
+    int val;        // kindがTK_NUMの場合, その数値
+    char *str;      // トークン文字列
 };
 // linked listを書いている??
+
+char *user_input;
 
 // 現在着目しているトークン
 Token *token;
 
-// エラーを報告するための関数
-// printfと同じ引数を取る: *fmt = "hoge%d\n"
-void error(char *fmt, ...){
+void error_at(char *loc, char *fmt, ...){
     va_list ap;
     va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n"); //"\n"が出力される. つまり改行をもうここに組み込んでしまおうと？
-    exit(1); // va_end()の代わりですかね.
-}// stderrにErrorメッセージが格納される？
 
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, ""); // print pos spaces.
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて
 // 真を返す。それ以外の場合には偽を返す。
-bool consume (char op){
-    if (token->kind != TK_RESERVED || token->str[0] != op){ //記号でないか、先頭の文字が指定した文字でなければfalse
+bool consume(char op){
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+    { //記号でないか、先頭の文字が指定した文字でなければfalse
         return false;
     }
     token = token->next;
@@ -53,7 +57,7 @@ bool consume (char op){
 
 void expect(char op){
     if (token->kind != TK_RESERVED || token->str[0] != op){
-        error("'%cではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     }
     token = token->next;
 }
@@ -62,19 +66,19 @@ void expect(char op){
 // それ以外の場合にはエラーを報告する。
 int expect_number(){
     if (token->kind != TK_NUM){
-        error("Not a number");
+        error_at(token->str, "Not a number");
     }
     int val = token->val;
     token = token->next;
     return val;
 }
 
-bool at_eof() {
+bool at_eof(){
     return token->kind == TK_EOF;
 }
 
 // 新しいトークンを作成してcurに繋げる
-Token *new_token(Tokenkind kind, Token *cur, char *str) {
+Token *new_token(Tokenkind kind, Token *cur, char *str){
     Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
     tok->str = str;
@@ -82,7 +86,8 @@ Token *new_token(Tokenkind kind, Token *cur, char *str) {
     return tok;
 }
 
-Token *tokenize(char *p){
+Token *tokenize(){
+    char *p = user_input;
     Token head;
     head.next = NULL;
     Token *cur = &head;
@@ -95,36 +100,39 @@ Token *tokenize(char *p){
         }
 
         if (*p == '+' || *p == '-'){
-            cur = new_token(TK_RESERVED, cur, p++); // 次の文字のポインタをcurにつなげる
+            cur = new_token(TK_RESERVED, cur, p++); // 文字のポインタをcurにつなげる.
+            /*
+            cur = new_token(TK_RESERVED, cur, p);
+            p++ 
+            と同じ. 後方で++するやつらしい...
+            だから, ここでは文字を入れて、次の数字のところにポインタを一つ進めている.
+            */
             continue;
         }
 
         if (isdigit(*p)){
             cur = new_token(TK_NUM, cur, p);
-            cur->val = strtol(p, &p, 10); // pの先頭が数字ならn(今は10)進法でlong型に変換. 違ったらその文字のポインタを&pに格納
+            cur->val = strtol(p, &p, 10); // pの先頭が数字ならn(今は10)進法でlong型に変換して文字のポインタを&pに格納
             continue;
         }
 
-        error("Couldn't tokenize...");
+        error_at(p, "Couldn't tokenize...");
     }
     new_token(TK_EOF, cur, p);
     return head.next;
 }
 
-
-
-int main(int argc, char **argv) {
-    if (argc != 2) {
+int main(int argc, char **argv){
+    if (argc != 2){
         fprintf(stderr, "Invalid char\n");
         return 1;
     }
 
     // トークナイズする
-    token = tokenize(argv[1]);
+    user_input = argv[1];
+    token = tokenize(); //global pointer varのtokenにheadをつなげる
 
     //　アセンブリの前半部分を出力
-    // char *p = argv[1];
-
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
@@ -132,7 +140,6 @@ int main(int argc, char **argv) {
     // 式の最初は数でなければならないので、それをチェックして
     // 最初のmov命令を出力
     printf("  mov rax, %d\n", expect_number());
-
 
     while (!at_eof()){
         if (consume('+')){
